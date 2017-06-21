c2a4a6e87c2dafc5cf2d426a5279c06d3cf1f0d6
baltimorekyz06 <baltimore.kyz@gmail.com>
2017-06-20 23:24:20 -0400

    tweets like and retweet count

--- src/__main__.py
+++ src/__main__.py
@@ -16,18 +16,23 @@ from credentials import (consumer_key, consumer_secret,
 
 
 def main():
-    coke_tweets, pepsi_tweets = get_tweets()
+    coke_tweets, pepsi_tweets, coke_likes, coke_retweets, pepsi_likes, pepsi_retweets = get_tweets()
     coke_lists, pepsi_lists = vectorize(coke_tweets), vectorize(pepsi_tweets)
 
     tweets = coke_tweets + pepsi_tweets
     word_lists = coke_lists + pepsi_lists
+    likes = coke_likes + pepsi_likes
+    retweets = coke_retweets + pepsi_retweets
 
-    for (tweet, words) in zip(tweets, word_lists):
+    for (tweet, words, like, retweet) in zip(tweets, word_lists, likes, retweets):
         diversity = lexical_diversity(words)
         sentiment = get_sentiment(tweet)
         print('=' * 79)
-        print(u'\n{}\n\nLexical Diversity: {}\nSentiment: {}\n'.format(
-            tweet, diversity, sentiment))
+        print(u'\n{}\n\nLexical Diversity: {}\nSentiment: {}'.format(
+            removeUnicode(tweet), diversity, sentiment))
+        print("number of likes = " + like)
+        print("number of retweets = " + retweet)
+        print("=" * 79)
 
     print('\nCoke Term Document Matrix:')
     display_tdm(build_tdm(coke_lists))
@@ -93,9 +98,18 @@ def get_sentiment(text):
 def get_tweets():
     """Return a corpus of tweets."""
     search = partial(twitter.search.tweets, count=25, lang='en')
-    coke_tweets = [status['text'] for status in search(q='Coke')['statuses']]
-    pepsi_tweets = [status['text'] for status in search(q='Pepsi')['statuses']]
-    return (coke_tweets, pepsi_tweets)
+
+    coke_handle = search(q='coke')['statuses']
+    coke_tweets = [status['text'] for status in coke_handle]
+    coke_likes = [str(status['favorite_count']) for status in coke_handle]
+    coke_retweets = [str(status["retweet_count"]) for status in coke_handle]
+
+    pepsi_handle = search(q='pepsi')['statuses']
+    pepsi_tweets = [status['text'] for status in pepsi_handle]
+    pepsi_likes = [str(status['favorite_count']) for status in pepsi_handle]
+    pepsi_retweets = [str(status["retweet_count"]) for status in pepsi_handle]
+
+    return (coke_tweets, pepsi_tweets, coke_likes, coke_retweets, pepsi_likes, pepsi_retweets)
 
 
 def vectorize(tweets):
@@ -145,6 +159,13 @@ def displayWordCount(tweets):
     print("Lexical Diversity")
     print(1.0*len(set(words))/len(words))
 
+def removeUnicode(text):
+    asciiText = ""
+    for char in text:
+        if(ord(char) < 128):
+            asciiText = asciiText + char
+
+    return asciiText
 
 if __name__ == '__main__':
     twitter = log_in()
